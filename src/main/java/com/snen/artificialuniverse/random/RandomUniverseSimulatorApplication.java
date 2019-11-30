1dd518fba3573b80827fbd2eeec13d0e07bc9283
Sergei Nenashev <snenashev@gmail.com>
2019-11-30 23:29:49 +0000

    Initial commit

---
+++ src/main/java/com/snen/artificialuniverse/random/RandomUniverseSimulatorApplication.java
@@ -0,0 +1,12 @@
+package com.snen.artificialuniverse.random;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomUniverseSimulatorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RandomUniverseSimulatorApplication.class, args);
+  }
+}
---
+++ src/test/java/com/snen/artificialuniverse/random/RandomUniverseSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snen.artificialuniverse.random;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomUniverseSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
